396c7dc29593658ea18154362f3fc2a15ae05203
Shanmuganand <12005921@bluetriton.com>
2024-03-16 16:56:08 +0530

    added food app folders

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Error() {
+  return (
+    <main className='error'>
+      <h1>Oops! something went wrong</h1>
+      <p>Failed to fetch meals data. Please try again later.</p>
+    </main>
+  )
+}
+
---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <main className='not-fount'>
+      <h1>Meal not Found</h1>
+      <p>Unfortunately, we could not find the requested page or meal data.</p>
+    </main>
+  )
+}
+
